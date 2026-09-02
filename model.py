dd08a73da5201d528635b0575971ab507199f9bf
Avik Das <avikd957@gmail.com>
2026-09-03 04:00:00 +0530

    Step 75: append_transition_to_buffer

--- model.py
+++ model.py
@@ -1396,8 +1396,29 @@ def create_replay_buffer(capacity):
         'capacity': int(capacity)
     }
 
-# Step 75 - append_transition_to_buffer (not yet solved)
-# TODO: implement
+# Step 75 - append_transition_to_buffer
+def append_transition_to_buffer(
+    buffer,
+    state,
+    action,
+    reward,
+    next_state,
+    done,
+    next_legal_mask
+):
+    """Append one (s, a, r, s', done, next_legal_mask) transition to the replay buffer."""
+    buffer['data'].append(
+        (
+            state,
+            action,
+            reward,
+            next_state,
+            done,
+            next_legal_mask
+        )
+    )
+
+    return buffer
 
 # Step 76 - cap_buffer_size_drop_oldest (not yet solved)
 # TODO: implement
