db7f321c78a2f9d64b086d9395fd90e4f836298b
Avik Das <avikd957@gmail.com>
2026-09-03 04:18:20 +0530

    Step 29: play_minimax_vs_random_matches

--- model.py
+++ model.py
@@ -435,6 +435,7 @@ def play_minimax_vs_random_matches(n_games, minimax_plays_x, rng):
             board = place_move(board, row, col, current_player)
 
             status = get_game_status(board)
+
             if status != 'ongoing':
                 outcomes.append(status)
                 break
